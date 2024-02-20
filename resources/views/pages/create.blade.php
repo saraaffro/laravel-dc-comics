93c346473af2b4034a6f931c3f42ef25eccd5761
saraaffro <saraaffro@gmail.com>
2024-02-20 09:59:49 +0100

    fix: if validation condition add to create.php

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,6 +4,15 @@
 @endsection
 @section('content')
     <h1>New Comic:</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
    
     <form action="{{ route('comics.store') }}" method="POST">
 
